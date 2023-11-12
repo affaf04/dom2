89700f796413f2990cc91f621b997c9497a4356c
affaf04 <affaftaouti@gmail.com>
2023-11-12 18:56:56 +0100

    edit

--- app.js
+++ app.js
@@ -1,17 +1,92 @@
+const mainEl = document.querySelector("main");
+
+mainEl.style.backgroundColor = "var(--main-bg)";
+
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+
+mainEl.classList.add("flex-ctr");
+
+const topMenuEl = document.getElementById("top-menu");
+
+topMenuEl.style.height = "100%";
+
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+topMenuEl.classList.add("flex-around");
+
 var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '#', subLinks: [
-      {text: 'all', href: '/catalog/all'},
-      {text: 'top selling', href: '/catalog/top'},
-      {text: 'search', href: '/catalog/search'},
-    ]},
-    {text: 'orders', href: '#' , subLinks: [
-      {text: 'new', href: '/orders/new'},
-      {text: 'pending', href: '/orders/pending'},
-      {text: 'history', href: '/orders/history'},
-    ]},
-    {text: 'account', href: '#', subLinks: [
-      {text: 'profile', href: '/account/profile'},
-      {text: 'sign out', href: '/account/signout'},
-    ]},
-  ];
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+menuLinks.forEach((link) => {
+  link.document.createElement("a");
+  link.setAttribute("href", link.href);
+  topMenuEl.appendChild(link);
+});
+
+var menuLinks = [
+  { text: "about", href: "/about" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
+];
+
+const subMenuEl = document.getElementById("sub-menu");
+
+subMenuEl.style.height = "100%";
+
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+const topMenuLinks = document.querySelectorAll("#top-menu a");
+
+let showingSubMenu = false;
+
+document.getElementById("top-menu").addEventListener("click", function (event) {
+  event.preventDefault();
+  if (event.target.tagName !== "A") {
+    return;
+
+  }  
+  console.log(event.target.textContent);
+  if (event.target.classList.contains('active')) {
+    event.target.classList.remove('active');
+    showingSubMenu = false;
+    subMenuEl.style.top = '0';
+    return;
+  }
+  topMenuLinks.forEach(link => {
+    link.classList.remove('active');
+  });
+  event.target.classList.add('active');
+
+});
+
